b264c217922a8a4256ec6e72911993093d6e535f
Abdullah Bin Rashid <abdullahbinrashid2005abr@gmail.com>
2023-05-05 03:17:16 +0500

    Fix Binary

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const ws = require('ws');
+const wsa = require('ws');
 
-const wss = new ws.WebSocketServer({ port: 80 });
+const wss = new wsa.WebSocketServer({ port: 80 });
 
 wss.on('connection', (ws) => {
     console.log('Client connected');
-    ws.on('message', (message) => {
-        console.log(`Received message => ${message}`);
+    ws.on('message', (message, isBinary) => {
+        if (isBinary) {
+            console.log(`Received Binary Message of ${message.length} bytes`);
+        } else {
+            console.log(`Received Message => ${message}`);
+        }
         wss.clients.forEach((client) => {
-
-            if (client !== ws && client.readyState === ws.OPEN) {
-                client.send(`${message}`)
+            if (client !== ws && client.readyState === wsa.OPEN) {
+                if (isBinary) {
+                    client.send(message, { binary: true });
+                    return;
+                }
+                client.send(`${message}`);
             }
         });
     });
