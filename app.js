616cff9b5a421199fa98c0265f652e7c0e0cb3d7
Abdullah Bin Rashid <abdullahbinrashid2005abr@gmail.com>
2024-08-25 13:00:14 +0000

    Add HTTPS, Switch to ES Modules

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-const app = require("express")();
-const expressWs = require("express-ws")(app);
-const wss = expressWs.getWss('/ws');
-require('dotenv').config();
+import https from "https";
+import http from "http";
 
+import express from "express"
+import expressWs from "express-ws";
+
+import fs from "fs";
+
+import cors from "cors";
+
+import { config } from "dotenv";
+config();
+
+const app = express();
+
+app.use(
+   express.urlencoded({
+       limit: "50mb",
+       extended: true,
+       parameterLimit: 50000,
+   })
+);
+
+app.use(cors({
+    origin: ["https://panda-chat.netlify.app", "localhost:8080"],
+    credentials: true
+}))
+
+let http_server = http.createServer(app);
+
+// Initialize WS
+const wss = expressWs(app, http_server).getWss("/ws");
+
+// Test Path
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
@@ -27,6 +56,28 @@ app.ws('/ws', (ws, req) => {
     });
 })
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server started on Port: ${process.env.PORT}`);
-});
+;(async () => {
+   
+    http_server.listen(process.env.HTTP_PORT, () => {
+        console.log(`Http Server is running on port ${process.env.HTTP_PORT}`);
+    });
+
+    if (process.env.HTTPS_PORT) {
+        if (!fs.existsSync('./keys/key.pem') || !fs.existsSync('./keys/cert.pem')) {
+            console.log("Https Keys are not present. Https Server will not run.");
+        } else {
+            var options = {
+                key: fs.readFileSync('./keys/key.pem'),
+                cert: fs.readFileSync('./keys/cert.pem')
+            };
+            try {
+                https.createServer(options, app).listen(process.env.HTTPS_PORT, () => {
+                    console.log(`Https Server is running on port ${process.env.HTTPS_PORT}`);
+                });
+            } catch (err) {
+                console.log("Https Server will not run.");
+                console.log((err instanceof Error) ? err.stack : "Error: " + err);
+            }
+        }
+    }
+})();
